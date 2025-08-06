88686225784d11dde095de9f7bca1dd8c70c766c
tminhscavi <TanMinh.Duong@scavi.com.vn>
2025-08-06 08:44:52 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+import PWA from 'next-pwa';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = PWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
