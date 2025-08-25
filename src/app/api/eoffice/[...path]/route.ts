084d6a6ce6c1e1b4a9e2178f6c0699593733d779
tminhscavi <TanMinh.Duong@scavi.com.vn>
2025-08-25 16:45:26 +0700

    fix

--- src/app/api/eoffice/[...path]/route.ts
+++ src/app/api/eoffice/[...path]/route.ts
@@ -1,11 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { api } from '@/utils/axios';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { [key: string]: string | string[] } },
-) {
-  const path = params.path as string[];
+export async function GET(req: NextRequest, context: any) {
+  const path = context.params.path as string[];
 
   const data = await api.get(`/Eoffice/${path.join('/')}`);
 
--- src/app/api/eoffice/factory/route.ts
+++ src/app/api/eoffice/factory/route.ts
@@ -1,8 +1,9 @@
-import { getFactories } from "@/services/factoryServices";
-import { NextRequest, NextResponse } from "next/server";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { getFactories } from '@/services/factoryServices';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest, { params }: { params: { path: string[] } }) {
-  const data = await getFactories()
+export async function GET(req: NextRequest, context: any) {
+  const data = await getFactories();
 
   return NextResponse.json(data);
 }
